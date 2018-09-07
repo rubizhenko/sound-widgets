4e350d5d4ba9e42a4fbf495652511264d90c45b0
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2018-09-08 01:54:41 +0300

    replaced message

--- queue.js
+++ queue.js
@@ -3,6 +3,7 @@
 var queue = [];
 var message = '<section class="js_widget"> <audio id="startMusic" src="sounds/start.mp3"></audio> <audio id="voiceMusic" src="sounds/main.mp3"></audio> <div class="container-fluid"> <div class="row text-center"> <div class="col-md-4 col-md-offset-4"><img src="img/source.gif"></div></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{name}}-{{amount}}грн</p></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{message}}</p></div></div></div></div></section><script>var startMusic=$("#startMusic").get(0);var voiceMusic=$("#voiceMusic").get(0);var playStartPromise=startMusic.play();if(playStartPromise!==undefined){playStartPromise.then(function(){console.log("startMusic без помилок");startMusic.onended=function(){console.log("Запуск озвучки");setTimeout(function(){playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}},500)}}).catch(function(error){console.log("startMusic з помилками");playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}})}</script>';
 
+
 var controller = makeWidgetsQueue(queue);
 var delay = 5000;
 var _total = 0;
