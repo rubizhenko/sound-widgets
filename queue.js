63a74d0b1376f88a694526c3418ec0107020e6e4
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2018-09-08 02:20:32 +0300

    Fixed bug with errors in all url

--- queue.js
+++ queue.js
@@ -1,14 +1,14 @@
 "use strict";
 
 var queue = [];
-var message = '<section class="js_widget"> <audio id="startMusic" src="sounds/start.mp3"></audio> <audio id="voiceMusic" src="sounds/main.mp3"></audio> <div class="container-fluid"> <div class="row text-center"> <div class="col-md-4 col-md-offset-4"><img src="img/source.gif"></div></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{name}}-{{amount}}грн</p></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{message}}</p></div></div></div></div></section><script>var startMusic=$("#startMusic").get(0);var voiceMusic=$("#voiceMusic").get(0);var playStartPromise=startMusic.play();if(playStartPromise!==undefined){playStartPromise.then(function(){console.log("startMusic без помилок");startMusic.onended=function(){console.log("Запуск озвучки");setTimeout(function(){playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}},500)}}).catch(function(error){console.log("startMusic з помилками");playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}})}</script>';
-
+var message =
+  '<section class="js_widget"> <audio id="startMusic" src="sounds/start.mp3"></audio> <audio id="voiceMusic" src="sounds/main.mp3"></audio> <div class="container-fluid"> <div class="row text-center"> <div class="col-md-4 col-md-offset-4"><img src="img/source.gif"></div></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{name}}-{{amount}}грн</p></div><div class="row text-center"> <div class="col-md-4 col-md-offset-4"> <p>{{message}}</p></div></div></div></div></section><script>var startMusic=$("#startMusic").get(0);var voiceMusic=$("#voiceMusic").get(0);var playStartPromise=startMusic.play();if(playStartPromise!==undefined){playStartPromise.then(function(){console.log("startMusic без помилок");startMusic.onended=function(){console.log("Запуск озвучки");setTimeout(function(){playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}},500)}}).catch(function(error){console.log("startMusic з помилками");playVoicePromise=voiceMusic.play();if(playVoicePromise!==undefined){playVoicePromise.then(function(){console.log("voiceMusic без помилок")}).catch(function(error){console.log("voiceMusic з помилками")})}})}</script>';
 
 var controller = makeWidgetsQueue(queue);
-var delay = 5000;
+var delay = 3000;
 var _total = 0;
-var fakeWebSocketMessage = setInterval(function () {
-  queue.push("<span class=\"huge\">" + ++_total + "</span> " + message);
+var fakeWebSocketMessage = setInterval(function() {
+  queue.push('<span class="huge">' + ++_total + "</span> " + message);
   controller.init();
 }, 500);
 
@@ -22,16 +22,23 @@ function makeWidgetsQueue(queue) {
         isNotExecuted = false;
         var firstWidget = queue[0];
         var canPlayCount = 0;
+        var errorSoundsCount = 0;
         $("body").append(firstWidget);
 
         var widget = $(".js_widget");
         var audioTags = widget.find("audio");
-        audioTags.on("canplay", function () {
+        audioTags.on("canplay", function() {
           canPlayCount++;
         });
-        audioTags.on("ended", function () {
+        audioTags.on("error", function() {
+          errorSoundsCount++;
+          if(errorSoundsCount === audioTags.length){
+            _this.showNext();
+          }
+        });
+        audioTags.on("ended", function() {
           canPlayCount--;
-          if (canPlayCount) {
+          if (canPlayCount === 0) {
             _this.showNext();
           }
         });
@@ -40,7 +47,7 @@ function makeWidgetsQueue(queue) {
     showNext: function showNext() {
       var _this2 = this;
 
-      setTimeout(function () {
+      setTimeout(function() {
         queue.shift();
         isNotExecuted = true;
         $("body").empty();
